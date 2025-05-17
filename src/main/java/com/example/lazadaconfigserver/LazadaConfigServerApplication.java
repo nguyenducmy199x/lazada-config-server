0e2e644fe5f761012a931830af6be2231361cd5f
admin <nguyenducmy9x@gmail.com>
2025-05-17 22:39:23 +0700

    commit

---
+++ src/main/java/com/example/lazadaconfigserver/LazadaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lazadaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazadaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazadaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lazadaconfigserver/LazadaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lazadaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazadaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
