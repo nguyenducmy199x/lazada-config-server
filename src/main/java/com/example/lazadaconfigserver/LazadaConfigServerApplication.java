91317159260f6d454721a0fd6e764cbeb3d29afe
admin <nguyenducmy9x@gmail.com>
2025-05-17 23:06:35 +0700

    commit

--- src/main/java/com/example/lazadaconfigserver/LazadaConfigServerApplication.java
+++ src/main/java/com/example/lazadaconfigserver/LazadaConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.example.lazadaconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class LazadaConfigServerApplication {
 
 	public static void main(String[] args) {
